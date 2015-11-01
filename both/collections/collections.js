0cb9003aff91b3aa0cdffeb083cbe4d0588bfcb9
Shannon <sboggs10@ivytech.edu>
2015-10-31 20:11:05 -0400

    TEMP FIX FOR:   (Added Authentiction packages, removed Insecure, now you cannot create or edit posts), now anyone can CRUD whether they are logged in or not. But it runs.
    Added Iron:Router
    Deleted hello.js
    Just before adding routing to split CRUD into views

--- both/collections/collections.js
+++ both/collections/collections.js
@@ -1,1 +1,17 @@
 Posts = new Mongo.Collection('posts');
+
+
+///// ***None of the following code is thought out or correct.
+////TODO: rewrite this code so it makes sense.
+Posts.allow({
+    insert: function (userId){
+        //user **should**  be logged in, but this allows anyone in.
+        return (userId === userId);
+    },
+    update: function(userId){
+    return(userId ===userId);
+},
+    remove:function(userId){
+        return(userId===userId);
+    }
+});
